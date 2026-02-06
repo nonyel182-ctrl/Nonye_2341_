dc60dcad5d00a8f50cfa6ceba8e432292bca8d65
nonyel182-ctrl <nonyelumnonyelum182@gmail.com>
2026-02-06 17:41:54 +0100

    Create Intro.sol

---
+++ Intro.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; // Solidity version
+contract Intro {
+    // Basic Types: boolean, uint, int, address, bytes 
+bool hasFavoriteNumber = true;
+uint256 favoriteNumber = 88;
+string favoriteNumberinText = "eighty-eight";
+int256 favoriteInt = -88;
+address myAddress =  0xa0A599bf8d8c425835387EcC8241DE88C2c0e223;
+bytes32 favoriteBytes32 = "cat";
+    
+    // Note: FavoriteeNumber gets initialized to 0 if no value is given (0 is the default value)
+    // While false is the default value for bool eg:
+uint256 favoriteeNumber;// 0 
+bool hasFavoriteeNumber;// false
+}
